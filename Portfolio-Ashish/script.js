83fb8ad34ee4afd0b2a522647c766cde5f76d78e
Ashish kumar <ashishoffil@gmail.com>
2024-10-28 12:16:15 +0530

    Add files via upload

---
+++ Portfolio-Ashish/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".dynamic-text",{
+    strings: ["Frontend Developer", "Backend Developer", "Web Designer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
